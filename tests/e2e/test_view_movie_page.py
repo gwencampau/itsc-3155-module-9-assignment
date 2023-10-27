0dae5b55161a5653d1164b7ed95b4bcc2d99a2ba
rmsze <rszeghy65@gmail.com>
2023-10-27 16:15:24 -0400

    Adding tests

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,1 +1,18 @@
 # TODO: Feature 4
+from app import app, movie_repository
+
+def test_get_single_movie(test_app):
+    movie1 = movie_repository.create_movie('Spirited Away', 'Hayao Miyazaki', 4)
+    response = test_app.get('/movies/' + str(movie1.movie_id))
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert 'Spirited Away' in data
+    assert 'Hayao Miyazaki' in data
+    assert '4' in data
+
+def test_get_single_movie_2(test_app):
+    response = test_app.get('/movies/')
+    assert response.status_code == 404
+
+    response = test_app.get('/movies/865465468')
+    assert response.status_code == 400
