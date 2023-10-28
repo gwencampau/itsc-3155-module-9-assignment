c2158569c690eb31cb2ec6eaae70ffddee679d3a
GavinGMC <90469511+GavinGMC@users.noreply.github.com>
2023-10-27 22:54:05 -0400

    tests for search

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,19 +1,23 @@
 # TODO: Feature 3
-from src.repositories.movie_repository import get_movie_repository
 
-
-def test_search_movies(test_app):
+def test_search_movies_page(test_app):
+    response = test_app.post('/movies/search', data={'title': 'Cool Movie'}, follow_redirects=True)
     response = test_app.get('/movies/search')
-    assert response.status_code == 200
 
-def test_searching(test_app):
-    movie = get_movie_repository() 
-    movie.create_movie("new movie", "gavin", 3)
-    movie_test = movie.get_movie_by_title
+    data = response.data.decode('utf-8')
+    
+    assert  'title' in data
 
-    response = test_app.get(f'/movies/{movie_test}', follow_redirects=True)
+    response = test_app.post('/movies/search', data={'title': 'Bad Movie'}, follow_redirects=True)
+    response = test_app.get('/movies/search')
 
-    assert response.status_code == 200
-    assert b'<td>new movie</td>' in response.data
-    assert b'<td> gavin </td>' in response.data
-    assert b'<td> 3 </td>' in response.data
+    data = response.data.decode('utf-8')
+
+    assert not('Some Movie' in data)
+
+    response = test_app.post('/movies/search', data={'title': ''}, follow_redirects=True)
+    response = test_app.get('/movies/search')
+
+    data = response.data.decode('utf-8')
+
+    assert '' in data
